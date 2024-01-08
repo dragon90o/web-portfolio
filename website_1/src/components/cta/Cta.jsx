d6ee78afe219183e28c752284bd48f269ec250d1
dragon90o <dravvt@gmail.com>
2024-01-08 20:41:11 +0100

    new file:   website_1/src/containers/index.js 	new file:   website_1/src/containers/possibility/Possibility.jsx 	new file:   website_1/src/containers/possibility/possibility.css 	new file:   website_1/src/containers/whatGPT3/WhatGPT3.jsx 	new file:   website_1/src/containers/whatGPT3/whatgpt3.css 	new file:   website_1/src/index.css 	new file:   website_1/src/index.js

---
+++ website_1/src/components/cta/Cta.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './cta.css';
+
+const Cta = () => {
+    return (
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-content">
+                <p>Request Early Access</p>
+                <h3>Register Today & Start Exploring The Possibilities </h3>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button type="button">get started</button>
+            </div>
+        </div>
+    )
+}
+
+export default Cta
